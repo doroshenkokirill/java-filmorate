3f5c8e3bba35baad8d61ad918d91d5e183980ca7
Kirill <doroshenkokirill56@yandex.ru>
2024-06-18 15:08:49 +0300

    add: добавил логирование

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -0,0 +1,36 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice("ru.yandex.practicum.filmorate.controller")
+public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleNotValidException(MethodArgumentNotValidException e) {
+        log.error("Ошибка - данные введены неверно");
+        return Map.of("Ошибка", "Команда не выполнена - данные неверны");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFoundException(NotFoundException e) {
+        log.error("Ошибка - запрашиваемых данных нет");
+        return Map.of("Ошибка", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleThrowable(Throwable e) {
+        log.error("Ошибка сервера");
+        return Map.of("Ошибка", "Что-то пошло не так");
+    }
+}
